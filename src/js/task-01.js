4b4d17945f256fe9ac30645c997a8c41f992b0bc
Alinaand <80483769+Alinaand@users.noreply.github.com>
2022-01-04 23:21:58 +0200

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const navEl = document.querySelectorAll('.item');
+const elList= navEl.length;
+console.log(navEl)
+console.log('Number of categories:',elList);
+navEl.forEach(element => {
+ console.log('Category:',element.querySelector('h2').textContent) ;
+ console.log ("Elements:",element.querySelectorAll('li').length )
+})
+;
+
