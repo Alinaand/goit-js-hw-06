8918c176d946de169ee0d909d1b81c913b1fdb4a
Alinaand <80483769+Alinaand@users.noreply.github.com>
2022-02-07 00:28:33 +0200

    edit

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn=document.querySelector('.change-color')
+const span=document.querySelector('.color')
+const body=document.querySelector('body')
+
+btn.addEventListener('click',changeColor);
+function changeColor(){
+const color=getRandomHexColor();
+body.style.backgroundColor = color;
+span.textContent=color;
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnCreate=document.querySelector('[data-create]');
+const btnDestroy=document.querySelector('[data-destroy]')
+const inpAmount=document.querySelector('input')
+console.log(inpAmount)
+inpAmount.addEventListener('input',(event)=>{
+ const amount=event.currentTarget.value
+  console.log(amount)
+  return amount
+})
+
+console.log(amount)
+// function createBoxes(amount){
+//   const box = document.createElement("div");
+//   box.classList.add('boxes')
+//   box.style.height='30px';
+//   box.style.width='30px'
+//   box.style.color=getRandomHexColor()
+// }
+// btnCreate.addEventListener('click',createBoxes)
+// console.log(box)
