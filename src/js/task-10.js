6a66d2113200d4bbdb5e9fdb84c67e2e185ac4f7
Alina Andreeva <alinaand7@gmail.com>
2022-02-10 18:46:40 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,20 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-const btnCreate=document.querySelector('[data-create]');
-const btnDestroy=document.querySelector('[data-destroy]')
-const inpAmount=document.querySelector('input')
-console.dir(inpAmount)
-let amount=0;
-btnCreate.addEventListener('click',(event)=>{
-  console.log('ghbdtn')
-  inpAmount.addEventListener('input', (e)=>{ let amount = e.currentTarget.value;})
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// const btnCreate=document.querySelector('[data-create]');
+// const btnDestroy=document.querySelector('[data-destroy]')
+// const inpAmount=document.querySelector('input')
+// console.dir(inpAmount)
+// let amount=0;
+// btnCreate.addEventListener('click',(event)=>{
+//   console.log('ghbdtn')
+//   inpAmount.addEventListener('input', (e)=>{ let amount = e.currentTarget.value;})
 
-  console.dir(amount)
+//   console.dir(amount)
 
-})
+// })
 
-console.log(amount)
+// console.log(amount)
 // function createBoxes(amount){
 //   const box = document.createElement("div");
 //   box.classList.add('boxes')
