8157c5f6ba64133981fb9e9d5f322d6e25c1164e
Alina Andreeva <alinaand7@gmail.com>
2022-02-11 18:10:10 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,21 +3,31 @@ function getRandomHexColor() {
 }
 const btnCreate=document.querySelector('[data-create]');
 const btnDestroy=document.querySelector('[data-destroy]')
-const inpAmount=document.querySelector('input')
+const inpAmount=document.querySelector('input');
+const boxDiv=document.querySelector('#boxes')
 console.log(inpAmount)
-inpAmount.addEventListener('input',(event)=>{
- const amount=inpAmount.value
-  console.log(amount)
-  return amount
-})
+inpAmount.addEventListener('input',(e)=>{
+ 
 
-// console.log(amount)
-// function createBoxes(amount){
-//   const box = document.createElement("div");
-//   box.classList.add('boxes')
-//   box.style.height='30px';
-//   box.style.width='30px'
-//   box.style.color=getRandomHexColor()
-// }
-// btnCreate.addEventListener('click',createBoxes)
-// console.log(box)
+const amount = inpAmount.value;
+  
+
+})
+console.log(amount)
+
+function createBoxes(amount){
+  const boxesColor=[];
+  for (let i = 0; i < amount;i+=1){ 
+  const box = document.createElement('div');
+  box.classList.add('boxes');
+  box.style.height=`30px+${i*10}`;
+  box.style.width=`30px+${i*10}`;
+  box.style.color=getRandomHexColor();
+boxesColor.push(box)}
+console.dir(boxesColor)
+return boxesColor
+
+}
+createBoxes(amount)
+// boxDiv.append(...boxesColor)
+btnCreate.addEventListener('click',createBoxes())
