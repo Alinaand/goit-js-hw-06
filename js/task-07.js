eee944694d05aeb1105afc344c3435edb654c399
Alinaand <80483769+Alinaand@users.noreply.github.com>
2022-02-13 20:46:57 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const formInput=document.querySelector('#font-size-control');
 const formSpan=document.querySelector('#text');
 const onformInputInput=(event)=>{
-    formSpan.style.fontSize=`${formInput.value}px`
+    formSpan.style.fontSize=`${formInput.value}px`  
 console.log(event.currentTarget.value)
 console.log(formSpan.style.fontSize)};
 formInput.addEventListener('input',onformInputInput)
